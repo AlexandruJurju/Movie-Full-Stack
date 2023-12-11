a6d17b37aeb33b094a455ffad2e5cc66af433997
Alex <alexandru.i.jurju@gmail.com>
2023-12-11 12:48:59 +0200

    added get movies on init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Movie} from "./model/movie";
+import {MovieService} from "./service/movie.service";
+import {HttpErrorResponse} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'Frontend';
+export class AppComponent implements OnInit {
+  public title: string = 'Frontend';
+  public movies: Movie[] = [];
+
+  constructor(private movieService: MovieService) {
+  }
+
+
+  ngOnInit() {
+    this.getMovies();
+  }
+
+  private getMovies(): void {
+    this.movieService.getMovies().subscribe({
+      next: (res: Movie[]) => {
+        this.movies = res
+      },
+      error: (error: HttpErrorResponse) => {
+        alert(error.message)
+      }
+    })
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './header/header.component';
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -12,9 +13,11 @@ import { HeaderComponent } from './header/header.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
