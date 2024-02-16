dbf46e4761f72352099d84ee0fa10b77f8969942
Alex <alexandru.i.jurju@gmail.com>
2024-02-16 09:31:55 +0200

    added security config for spring security

---
+++ SpringMovie/src/main/java/com/example/springmovie/security/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.example.springmovie.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+    // TODO: testing, remove when done
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers(HttpMethod.GET, "/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/**").permitAll()
+                        .requestMatchers(HttpMethod.PUT, "/**").permitAll()
+                        .requestMatchers(HttpMethod.DELETE, "/**").permitAll()
+                        .anyRequest().authenticated()
+                );
+        return http.build();
+    }
+}
+
