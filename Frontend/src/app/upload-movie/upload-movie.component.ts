eece4034b41cfa1de908929268c788e3e4563695
Alex <alexandru.i.jurju@gmail.com>
2024-03-13 14:11:24 +0200

    started upload movie

--- Frontend/src/app/upload-movie/upload-movie.component.ts
+++ Frontend/src/app/upload-movie/upload-movie.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormsModule, NgForm} from "@angular/forms";
+import {MovieService} from "../service/movieService";
+import {Movie} from "../model/movie";
 
 @Component({
   selector: 'app-upload-movie',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './upload-movie.component.html',
   styleUrl: './upload-movie.component.css'
 })
 export class UploadMovieComponent {
+
+
+  constructor(private movieService: MovieService) {
+  }
+
+  onSubmit(form: NgForm) {
+    const movie: Movie = form.value;
+    this.movieService.saveMovie(movie).subscribe({
+      next: (response) => {
+        console.log('Upload successful', response);
+      },
+      error: (error) => {
+        console.error('Upload failed', error);
+      }
+    });
+  }
 
 }
