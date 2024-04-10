bafdc9ff9c106b0c16657ce66c139375a7c0db36
Alexandru.I.Jurju <alexandru.i.jurju@gmail.com>
2024-04-10 08:42:51 +0300

    check local storage for token on init

--- Frontend/src/app/service/movie.service.ts
+++ Frontend/src/app/service/movie.service.ts
@@ -2,51 +2,45 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {Movie} from "../model/movie";
+import {BASE_URL} from "../config";
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieService {
 
-  private readonly baseUrl = 'http://localhost:8080/api/v1/movie'; // Use readonly instead of private
-
   constructor(private http: HttpClient) {
   }
 
-  // TODO: implement all methods from service
-  // TODO: add page for edit
-  // TODO: add page for upload
-  // TODO: add page for searching
-  // TODO: add filtering
   findAllMoviesUnpaged(): Observable<Movie[]> {
-    return this.http.get<Movie[]>(this.baseUrl + '/unpaged'); // Append '/unpaged' to base URL
+    return this.http.get<Movie[]>(`${BASE_URL}/api/v1/movie/unpaged`);
   }
 
   findMovieById(movieId: number): Observable<Movie> {
-    return this.http.get<Movie>(`${this.baseUrl}/${movieId}`)
+    return this.http.get<Movie>(`${BASE_URL}/api/v1/movie/${movieId}`);
   }
 
   findMoviesByReleaseStatus(status: string): Observable<any> {
-    return this.http.get(`${this.baseUrl}/status/${status}`);
+    return this.http.get(`${BASE_URL}/api/v1/movie/status/${status}`);
   }
 
   findMoviesByReleaseYear(year: number): Observable<any> {
-    return this.http.get(`${this.baseUrl}/year/${year}`);
+    return this.http.get(`${BASE_URL}/api/v1/movie/year/${year}`);
   }
 
   saveMovie(movie: Movie): Observable<Movie> {
-    return this.http.post<Movie>(this.baseUrl, movie)
+    return this.http.post<Movie>(`${BASE_URL}/api/v1/movie`, movie);
   }
 
   deleteMovie(movieId: number): Observable<void> {
-    return this.http.delete<void>(`${this.baseUrl}/${movieId}`);
+    return this.http.delete<void>(`${BASE_URL}/api/v1/movie/${movieId}`);
   }
 
   updateMovie(movie: Movie): Observable<Movie> {
-    return this.http.put<Movie>(this.baseUrl, movie);
+    return this.http.put<Movie>(`${BASE_URL}/api/v1/movie`, movie);
   }
 
   deleteMovieById(movieId: number): Observable<any> {
-    return this.http.delete(`${this.baseUrl}/${movieId}`);
+    return this.http.delete(`${BASE_URL}/api/v1/movie/${movieId}`);
   }
 }
