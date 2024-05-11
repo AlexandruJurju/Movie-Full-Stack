40526695e40d638baa8874e24a2b607a0e062250
Alexandru.I.Jurju <alexandru.i.jurju@gmail.com>
2024-05-11 13:38:33 +0300

    added genre save

--- Movie-Frontend/src/app/app.routes.ts
+++ Movie-Frontend/src/app/app.routes.ts
@@ -8,18 +8,44 @@ import {MovieEditComponent} from "./pages/movie-edit/movie-edit.component";
 import {GenreListComponent} from "./pages/genre-list/genre-list.component";
 import {GenreEditComponent} from "./pages/genre-edit/genre-edit.component";
 import {ErrorComponent} from "./pages/error/error.component";
+import {GenreSaveComponent} from "./pages/genre-save/genre-save.component";
 
 
 export const routes: Routes = [
-  {path: 'user-register', component: UserRegisterComponent},
-  {path: 'user-login', component: UserLoginComponent},
+  {
+    path: 'user-register',
+    component: UserRegisterComponent
+  },
+  {
+    path: 'user-login',
+    component: UserLoginComponent
+  },
 
-  {path: 'movie-list', component: MovieListComponent},
-  {path: 'movie-details/:id', component: MovieDetailsComponent},
-  {path: 'movie-edit/:id', component: MovieEditComponent},
+  {
+    path: 'movie-list',
+    component: MovieListComponent
+  },
+  {
+    path: 'movie-details/:id',
+    component: MovieDetailsComponent
+  },
+  {
+    path: 'movie-edit/:id',
+    component: MovieEditComponent
+  },
 
-  {path: 'genre-list', component: GenreListComponent},
-  {path: 'genre-edit/:id', component: GenreEditComponent},
+  {
+    path: 'genre-list',
+    component: GenreListComponent
+  },
+  {
+    path: 'genre-edit/:id',
+    component: GenreEditComponent
+  },
+  {
+    path: 'genre-save',
+    component: GenreSaveComponent,
+  },
 
   {path: 'home', component: HomeComponent},
 
